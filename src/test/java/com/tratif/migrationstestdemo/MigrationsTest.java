d0674bfe2235cf06c7ed04d1dbbef0f5369feb19
Tomasz Kaczmarzyk <tomasz+github@kaczmarzyk.net>
2017-12-07 15:42:11 +0100

    added migrations and diff filtering

--- src/test/java/com/tratif/migrationstestdemo/MigrationsTest.java
+++ src/test/java/com/tratif/migrationstestdemo/MigrationsTest.java
@@ -1,9 +1,14 @@
 package com.tratif.migrationstestdemo;
 
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.sql.DataSource;
 
@@ -21,11 +26,16 @@ import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.diff.DiffResult;
+import liquibase.diff.Difference;
+import liquibase.diff.ObjectDifferences;
 import liquibase.diff.compare.CompareControl;
 import liquibase.diff.output.report.DiffToReport;
 import liquibase.exception.DatabaseException;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.structure.DatabaseObject;
+import liquibase.structure.core.Catalog;
+import liquibase.structure.core.DataType;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -50,11 +60,60 @@ public class MigrationsTest {
 
         DiffResult diff = getLiquibase().diff(getDb(reference), getDb(liquibaseCon), compareControl());
 
+        filter(diff);
+        
         System.out.println("====[ Diff Result ]====");
         new DiffToReport(diff, System.out).print();
 
         assertTrue(MISMATCH_MESSAGE, diff.areEqual());
     }
+
+	private void filter(DiffResult diff) {
+		filterUnexpectedObjects(diff);
+		filterChangedObjects(diff);
+	}
+
+	/**
+	 * not needed for this project, just demonstrating how filtering works
+	 */
+	private void filterChangedObjects(DiffResult diff) {
+		Map<DatabaseObject, ObjectDifferences> changedObjects = diff.getChangedObjects();
+		Map<DatabaseObject, ObjectDifferences> changedObjectsFiltered = changedObjects.entrySet().stream()
+				.filter(entry -> {
+					ObjectDifferences differences = entry.getValue();
+			
+					Difference typeDifference = differences.getDifference("type");
+					if (typeDifference != null) {
+						if (((DataType) typeDifference.getComparedValue()).getTypeName().equals("BIGINT") &&
+								((DataType) typeDifference.getReferenceValue()).getTypeName().equals("BIGINT")) {
+							// ignoring BIGINT size
+							differences.removeDifference("type");
+						}
+					}
+			
+					return differences.hasDifferences();
+				})
+				.filter(entry -> entry.getKey().getClass() != Catalog.class) // ignoring catalog name differences
+				.collect(toMap(Entry::getKey, Entry::getValue));
+		changedObjects.clear();
+		changedObjects.putAll(changedObjectsFiltered);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void filterUnexpectedObjects(DiffResult diff) {
+		Set<DatabaseObject> unexpectedObjects = (Set<DatabaseObject>) diff.getUnexpectedObjects();
+		Set<DatabaseObject> unexpectedObjectsFiltered = unexpectedObjects.stream()
+				.filter(obj -> {
+					// ignore liquibase changelog tables:
+					if (obj.toString().contains("DATABASECHANGELOG")) {
+						return false;
+					}
+					return true;
+				})
+				.collect(toSet());
+		unexpectedObjects.clear();
+		unexpectedObjects.addAll(unexpectedObjectsFiltered);
+	}
 
 	private CompareControl compareControl() {
 		CompareControl compareControl = new CompareControl();
