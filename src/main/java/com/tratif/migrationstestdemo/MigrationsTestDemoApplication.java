e204181fdf517d8bb078381a1735bf2303b8d027
Tomasz Kaczmarzyk <tomasz+github@kaczmarzyk.net>
2017-12-07 14:18:23 +0100

    initial commit

---
+++ src/main/java/com/tratif/migrationstestdemo/MigrationsTestDemoApplication.java
@@ -0,0 +1,12 @@
+package com.tratif.migrationstestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationsTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationsTestDemoApplication.class, args);
+	}
+}
